6b762ee7bd6319c1f735844cfe22a041189e6ce2
2869955900 <100764356+2869955900@users.noreply.github.com>
2025-07-10 19:10:47 +0800

    Update 程序APP.py

--- 程序APP.py
+++ 程序APP.py
@@ -19,7 +19,7 @@ default_values = {
 
 # 创建函数，用于预测风险评分以及绘制累计风险函数和生存函数
 def predict_risk(SDMA_ADMA_pos_140, Thymine_pos_150, Phosphocreatine_neg_067, 
-                 Proline_pos_132, Glycerophosphorylcholine_pos_080, Guanidineacetic acid_pos_087):
+                 Proline_pos_132, Glycerophosphorylcholine_pos_080, Guanidineacetic_acid_pos_087):
     
     # 使用准确的列名，确保与训练时一致
     correct_columns = ["SDMA-ADMA_pos-140", "Thymine_pos-150", "Phosphocreatine_neg-067",
